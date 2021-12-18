746f6331d80d57783f149ccf00b4ba3b0e92710e
nullmaxwell <nullmaxwell@protonmail.com>
2021-12-17 18:54:37 -0600

    Added first iteration of landing page.

--- src/views/landing.py
+++ src/views/landing.py
@@ -3,15 +3,140 @@ from dash import html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 
+# Dash mandatory imports
 from app import app
+
+# Components import
+from components import components
 
 
 def serve_layout() -> list:
     """
     Defines and returns the layout for the landing page.
     """
+    container = dbc.Container(
+        id="main-container", children=[html.Div(children=[getMainPanel()])]
+    )
 
-    return [getInfoComponent()]
+    return [container]
+
+
+def getMainPanel() -> any:
+    """
+    Defines and returns the main content components.
+    """
+
+    return dbc.Container(
+        id="content",
+        children=[
+            dbc.Col(id="info-bar", children=[], width=12),
+            dbc.Col(id="left-pane", children=[getLeftPaneContent()], width=10),
+            dbc.Col(id="image-pane", children=[], width=2),
+        ],
+    )
+
+
+def getInfoBarContent() -> any:
+    """
+    Defines and returns the content of the
+    From left to right I would like the following items.
+
+    Day of week
+    Date
+    Time
+    Sunrise
+    Sunset
+    """
+    return None
+
+
+def getLeftPaneContent() -> any:
+    """
+    Defines and returns the elements within the left hand side pane.
+    In order I would like the following items:
+
+    Title bar -- centered (maybe this can be the time)
+    Subtext -- centered (welcome message)
+
+    Row of 5 columns
+    """
+    return dbc.Row(
+        id="categories-container",
+        children=[
+            components.createCategory(
+                title="Category1",
+                bookmarks=[
+                    (
+                        "images/io.polymail.ios-large.png",
+                        "Test 1",
+                        "https://google.com",
+                    ),
+                    (
+                        "images/io.polymail.ios-large.png",
+                        "Test 2",
+                        "https://google.com",
+                    ),
+                ],
+            ),
+            components.createCategory(
+                title="Category2",
+                bookmarks=[
+                    (
+                        "images/io.polymail.ios-large.png",
+                        "Test 1",
+                        "https://google.com",
+                    ),
+                    (
+                        "images/io.polymail.ios-large.png",
+                        "Test 2",
+                        "https://google.com",
+                    ),
+                ],
+            ),
+            components.createCategory(
+                title="Category3",
+                bookmarks=[
+                    (
+                        "images/io.polymail.ios-large.png",
+                        "Test 1",
+                        "https://google.com",
+                    ),
+                    (
+                        "images/io.polymail.ios-large.png",
+                        "Test 2",
+                        "https://google.com",
+                    ),
+                ],
+            ),
+            components.createCategory(
+                title="Category4",
+                bookmarks=[
+                    (
+                        "images/io.polymail.ios-large.png",
+                        "Test 1",
+                        "https://google.com",
+                    ),
+                    (
+                        "images/io.polymail.ios-large.png",
+                        "Test 2",
+                        "https://google.com",
+                    ),
+                    (
+                        "images/io.polymail.ios-large.png",
+                        "Test 2",
+                        "https://google.com",
+                    ),
+                ],
+            ),
+        ],
+    )
+
+
+def getRightPaneContent() -> any:
+    """
+    Defines and returns the elements within the right hand side pane.
+    """
+    return None
 
 
 def getInfoComponent() -> any:
